2d60319df6c29e73f4a4dad128ee79dc3131e5de
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-02-08 12:36:32 -0300

    numeros pares

---
+++ src/Algoritmos/NumerosPares.js
@@ -0,0 +1,13 @@
+const parArray = [];
+
+function numerosPares() {
+  for (var i = 0; i <= 100; i++) {
+    if (i === 0) {
+    } else if (i % 2 === 0) {
+      parArray.push(i);
+    }
+  }
+  console.log(parArray);
+}
+
+numerosPares();
