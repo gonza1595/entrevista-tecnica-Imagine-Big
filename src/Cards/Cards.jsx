34f7cd35c36809148159526b3dd7a704d07d3c73
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-02-10 19:22:27 -0300

    fix bugs

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Card from "../Card/Card";
+import "./Cards.css";
 
 export default function Cards({ nameSearch, productos, setNameSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = (event) => {
     setNameSearch(event.target.value);
+    setSearchTerm("");
   };
 
   const handleSubmit = (event) => {
@@ -24,10 +26,7 @@ export default function Cards({ nameSearch, productos, setNameSearch }) {
     : productos;
 
   return (
-    <div
-      className="bg-secondary bg-gradient container  "
-      // style={{ height: "100vh" }}
-    >
+    <div className="bg-secondary bg-gradient container pt-5 ">
       <div className="container pt-5 pb-3">
         <div className="row">
           <div className="col-md-12">
@@ -36,6 +35,7 @@ export default function Cards({ nameSearch, productos, setNameSearch }) {
                 type="text"
                 className="form-control input-text"
                 placeholder="Buscar productos...."
+                value={nameSearch}
                 onChange={(event) => handleSearch(event)}
               />
               <div className="input-group-append">
@@ -51,29 +51,33 @@ export default function Cards({ nameSearch, productos, setNameSearch }) {
           </div>
         </div>
       </div>
-      <div className="row">
-        {filteredProducts.length > 0 ? (
-          <>
-            {filteredProducts.map((product) => (
-              <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-3.5 col-sm-12 col-xs-12 p-3">
-                <Card
-                  img={product.img}
-                  name={product.name}
-                  id={product.id}
-                  price={product.price}
-                />
+      <div className="row ">
+        <div className="col-12 ">
+          {filteredProducts.length > 0 ? (
+            <>
+              <div className="row ">
+                {filteredProducts.map((product) => (
+                  <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-3.5 col-sm-12 col-xs-12 p-5">
+                    <Card
+                      img={product.img}
+                      name={product.name}
+                      id={product.id}
+                      price={product.price}
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
-            <div>
-              <hr className="hr hr-blurry p-1" />
-              {filteredProducts.length > 1
-                ? `${filteredProducts.length} productos encontrados`
-                : `${filteredProducts.length} producto encontrado`}
-            </div>
-          </>
-        ) : (
-          <div>No products found</div>
-        )}
+              <hr className="hr hr-blurry p-1 " />
+              <div className="text-center">
+                {filteredProducts.length > 1
+                  ? `${filteredProducts.length} productos encontrados`
+                  : `${filteredProducts.length} producto encontrado`}
+              </div>
+            </>
+          ) : (
+            <div className="text-center">No se encontraron productos</div>
+          )}
+        </div>
       </div>
     </div>
   );
